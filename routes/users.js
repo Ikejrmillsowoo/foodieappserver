ebdedd4c1841205aa7991d8a5b695588586609c4
poppymills2@gmail.com <poppymills2@gmail.com>
2021-07-31 21:54:39 -0400

    mongoose initiated

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
+const User = require("../models/users");
+
+var userRouter = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+userRouter
+  .route("/")
+  .get((req, res, next) => {
+    User.find()
+      .then((users) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(users);
+      })
+      .catch((err) => next(err));
+  })
+  .post((req, res, next) => {
+    User.create(req.body)
+      .then((user) => {
+        console.log("User Created ", user);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(user);
+      })
+      .catch((err) => next(err));
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /users");
+  })
+  .delete((req, res, next) => {
+    User.deleteMany()
+      .then((user) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(user);
+      })
+      .catch((err) => next(err));
+  });
 
-module.exports = router;
+module.exports = userRouter;
